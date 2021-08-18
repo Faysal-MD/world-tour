d7865bed616673e7ab3ad6db3dcd1b1ff2122349
Faysal075 <faysalmahmud735@gmail.com>
2021-08-18 12:02:28 -0500

    foreach-used

--- app.js
+++ app.js
@@ -10,26 +10,30 @@ countryApi();
 
 const displayData = (data) => {
     const CountryParentNode = document.getElementById('countries');
-    for (let i = 0; i < data.length; i++) {
-        const country = data[i];
-
+    data.forEach((country) => {
         const countriesDiv = document.createElement('div');
         countriesDiv.className = 'country';
-        // const countryName = document.createElement('h1');
-        // countryName.innerText = country.name;
 
-        // const capital = document.createElement('p');
-        // capital.innerText = country.capital;
-
-        // countryDiv.appendChild(countryName);
-        // countryDiv.appendChild(capital);
-        
         const countryInfo = `
             <h3 class="country-name">${country.name}</h3>
             <p class="capital-name">${country.capital}</p>
-        `
+        `;
         countriesDiv.innerHTML = countryInfo;
 
         CountryParentNode.appendChild(countriesDiv);
-    }
+    })
+    // for (let i = 0; i < data.length; i++) {
+    //     const country = data[i];
+
+    //     const countriesDiv = document.createElement('div');
+    //     countriesDiv.className = 'country';
+
+    //     const countryInfo = `
+    //         <h3 class="country-name">${country.name}</h3>
+    //         <p class="capital-name">${country.capital}</p>
+    //     `;
+    //     countriesDiv.innerHTML = countryInfo;
+
+    //     CountryParentNode.appendChild(countriesDiv);
+    // }
 }
